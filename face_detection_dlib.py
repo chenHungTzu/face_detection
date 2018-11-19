a74fd0c26cf70ad7f244d974b5229851d1959dda
chenhungtzu <motx2152000@gmail.com>
2018-11-20 00:24:40 +0800

    comment

--- face_detection_dlib.py
+++ face_detection_dlib.py
@@ -1,13 +1,11 @@
 import dlib
 import cv2 
-import imutils 
 import uuid
 import numpy as np
 from PIL import Image
 from traning_module_CNN import classifier
 from traning import training_set
 from runpy import run_path
-import os
 import scipy.misc
 
 # 開啟影片檔案
@@ -25,7 +23,7 @@ while(cap.isOpened()):
 
   # 取出所有偵測的結果
   for i, d in enumerate(face_rects):
-      
+    #取得結果的長寬高座標點
     x1 = d.left()
     y1 = d.top()
     x2 = d.right()
@@ -38,21 +36,41 @@ while(cap.isOpened()):
   #搜集資訊
   if img.any():   
     try:
+
+      #對於圖片截圖
       roi = img[y1:y2, x1:x2]
+
+      #存取圖片（測試用）
       ##filename = 'data/' + str(uuid.uuid4()) + '.png'
+
+      #圖片寫入（測試用）
       ##cv2.imwrite(filename,roi);
+
+      #透過spicy 將npArray 轉成圖片格式
       im = scipy.misc.toimage(roi,255)
+
+      #轉為64X64 像素
       crim = im.resize((64,64))
+
+      #???
       test_image = np.expand_dims(crim, axis = 0)
+
+      #送入模型判斷
       result = classifier.predict_classes(test_image)
  
+      #取得結果指標
       index = result.item()
+
+      #將dict_array （key list）轉為list
       dis_arr = list(training_set.class_indices.keys())
 
+      #將對應的結果分類讀出
       cv2.putText(frame, str(dis_arr[index]), (x1, y1) , cv2.FONT_HERSHEY_DUPLEX,
                   0.7, (255, 255, 255), 1, cv2.LINE_AA)
             
+      #並顯示在畫面上
       cv2.imshow("Face Detection", frame)
+
     except :
       print("An exception occurred")
    
--- traning_file_structure.py
+++ traning_file_structure.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from shutil import copyfile
-import parameter as e
+import parameter as p
 import os
 import numpy as np
 from PIL import Image
@@ -8,32 +8,36 @@ from PIL import Image
 def copyFileToDst(dataset, datafolder, srcfolder):
     for f in dataset:
 
+            #排除不必要的檔案
             if f == '.DS_Store':
                 continue
             else:
-                src = srcfolder+ f
-                dst = datafolder+srcfolder+f
+                src = srcfolder + f
+                dst = datafolder + srcfolder + f
                 im = Image.open(src)
                 os.remove(src)
+                #resize  64X64  像素
                 crim = im.resize((64,64))
                 crim.save(src)
                 copyfile(src, dst)
 
+#不存在就新增
 def existOrCreate(path) :
    if os.path.exists(path)  == False :
        os.mkdir(path)
 
-
-for i, d in enumerate(e.members):
+#根據目錄內的圖片讚單,搬移資料
+for i, d in enumerate(p.members):
     existOrCreate(d)
     info = os.listdir(d)
-    print(info)
+
+    #分離測試資料集與實際資料集
     train , test = train_test_split(info , test_size= 0.2 , random_state= 42)  
-    existOrCreate(e.train_path_prefix)
-    existOrCreate(e.test_path_prefix)
-    existOrCreate(e.train_path_prefix + '/' + d + '/')
-    existOrCreate(e.test_path_prefix + '/' + d + '/')
-    copyFileToDst(train, e.train_path_prefix + '/', d + '/')
-    copyFileToDst(test, e.test_path_prefix + '/', d + '/')
+    existOrCreate(p.train_path_prefix)
+    existOrCreate(p.test_path_prefix)
+    existOrCreate(p.train_path_prefix + '/' + d + '/')
+    existOrCreate(p.test_path_prefix + '/' + d + '/')
+    copyFileToDst(train, p.train_path_prefix + '/', d + '/')
+    copyFileToDst(test, p.test_path_prefix + '/', d + '/')
   
 
